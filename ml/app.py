605c21296abcb3db3af764ad98af76f56d8f9545
Debopriyo Roy <debopriyoroy2010@gmail.com>
2024-08-31 22:26:03 +0530

    Chatbot Creation

--- ml/app.py
+++ ml/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from chatbot import generate_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/chat',methods=['POST'])
+def chat():
+    data=request.get_json()
+    question=data.get('question','')
+    
+    response=generate_response(question)
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
