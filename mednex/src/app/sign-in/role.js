62eca5f2a9820cc9f4e6d4564c0470d2820f001a
samalsubrat <samalsubrat919@gmail.com>
2024-09-01 15:02:47 +0530

    role fix

--- mednex/src/app/sign-in/role.js
+++ mednex/src/app/sign-in/role.js
@@ -3,12 +3,12 @@ let role = "";
 
 // role.js
 export function setRole(role) {
-  localStorage.setItem("userRole", role);
+  localStorage.setItem("userType", role);
 }
 
 export function getRole() {
   // Retrieve the role from localStorage
-  const role = localStorage.getItem("userRole");
+  const role = localStorage.getItem("userType");
 
   // Return the role or null if not found
   return role ? role : null;
