3c8bb08ce5b2cff2daba8c19e764cd5065e8bc16
samalsubrat <samalsubrat919@gmail.com>
2024-09-11 23:43:37 +0530

    page setup

--- mednex/src/app/page.js
+++ mednex/src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       {/* <Image src="/landingpage.png" alt="landing page" width={3000} height={9000} className="p-0 m-0 w-full"/> */}
-      <div className="p-0 m-0">
+      <div className="p-0 m-0 flex flex-col justify-center items-center">
         <div className="relative flex h-[95vh] w-full flex-col items-center justify-center overflow-hidden bg-green-main space-y-2">
           <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
             Explore Modern
@@ -18,7 +18,11 @@ export default function Home() {
           </p>
           <Ripple className="opacity-70" />
         </div>
-        <div className="bg-[#D4E4DA] rounded-xl p-10 absolute bottom-0 w-full"></div>
+        <div className="bg-[#D4E4DA] rounded-xl p-6 absolute bottom-7 w-full"></div>
+        <div className="bg-[#D4E4DA]  w-full py-4">
+          <p className="font-semibold text-black text-center leading-[60px] tracking-[-3px] text-5xl"> Effortless Advanced<br/>Hospital Management.</p>
+         
+        </div>
       </div>
     </>
   );
